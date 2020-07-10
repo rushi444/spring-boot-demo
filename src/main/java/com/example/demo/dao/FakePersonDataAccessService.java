55e228915f4ba70a8ca8aa1feb0fef2f950af10e
Rushi <rushi44@gmail.com>
2020-07-09 19:08:41 -0700

    get requests for person working

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -2,17 +2,44 @@ package com.example.demo.dao;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import com.example.demo.model.Person;
 
-public class FakePersonDataAccessService implements PersonDao{
+import org.springframework.stereotype.Repository;
+
+@Repository("fakeDao")
+public class FakePersonDataAccessService implements PersonDao {
 
     private static List<Person> DB = new ArrayList<>();
-    
+
     @Override
-    public int insertPerson(UUID id, Person person){
+    public int insertPerson(UUID id, Person person) {
         DB.add(new Person(id, person.getNamString()));
+        return 0;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        // TODO Auto-generated method stub
+        return DB.stream().filter(person -> person.getId().equals(id)).findFirst();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id) {
+        // TODO Auto-generated method stub
         return 0;
     }
 }
